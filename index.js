09bfc64d2a44570d5c533c3d48c6f9671e02cb5d
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-01-23 15:34:03 -0600

    added initial data

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  projects: require('./data/projects.json'),
+  categories: require('./data/categories.json'),
+  collaborationTypes: require('./data/collaborationTypes.json'),
+  labels: require('./data/labels.json')
+}
